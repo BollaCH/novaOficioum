2d9dc41b76177f1e72dc9b7ee988c29cff1871d3
Diogo Chandoha <50423349+BollaCH@users.noreply.github.com>
2023-05-27 14:28:25 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+const formEl = document.querySelector(".form");
+const formBtn = document.querySelector("#faleconosco");
+function remove() {
+  headerEl.classList.remove("nav-open")
+}
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+});
+
+document.querySelectorAll('.nav-bar__navigation--link').forEach(item => {
+  item.addEventListener('click', event => {
+    remove()
+  })
+})
+
+formBtn.addEventListener("click", function () {
+  formEl.classList.toggle("form-open");
 });
