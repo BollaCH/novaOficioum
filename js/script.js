848e9e5429af7af33ccbb9cbfc1280344821249b
Diogo Chandoha <50423349+BollaCH@users.noreply.github.com>
2023-06-14 13:35:02 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -40,3 +40,18 @@ form.addEventListener("submit", () => {
 form.addEventListener("submit", () => {
   sucessAlert.classList.add("show")
 })
+
+const allLinks = document.querySelectorAll(".scroll-nav");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
